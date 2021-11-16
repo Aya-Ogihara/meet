ff4e5d49a151a73d52833c476009ddff2fe1a33d
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-16 15:08:25 +1300

    Implement EventList test

--- src/EventList.js
+++ src/EventList.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const EventList = () => {
+//Component
+import Event from './Event';
+
+const EventList = (props) => {
+
+  const { events } = props;
+
   return (
     <ul className="EventList">
-      
+      {events.map(event => 
+        <li key={event.id}>
+          <Event event={event} />
+        </li>
+        )}
     </ul>
-  )
+  );
 }
 
 export default EventList;
