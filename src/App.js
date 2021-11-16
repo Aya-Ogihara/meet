3c71f1e3d40bf1a4f77d64279cd2082fc42de25d
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-16 14:50:31 +1300

    Implement App.test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Component
+import EventList from './EventList';
 
 function App() {
   return (
     <div className="App">
-      
+      <EventList />
     </div>
   );
 }
---
+++ src/EventList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventList = () => {
+  return (
+    <ul className="EventList">
+      
+    </ul>
+  )
+}
+
+export default EventList;
