9167481e93bb96b343b1868c29481a30afbe2dfc
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-16 21:34:16 +1300

    Add CitySearch test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 // Component
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 function App() {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
