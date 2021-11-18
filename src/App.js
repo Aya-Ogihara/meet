880ddffbd3f40170be54873b4b5d794ef0a34852
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-18 21:57:19 +1300

    Add NumberOfEvents files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // Component
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
   );
 }
---
+++ src/NumberOfEvents.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NumberOfEvents extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
