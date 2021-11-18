72b318316a054958cd637a662e8c284e35a837c3
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-19 12:17:46 +1300

    Imprement NumberOfEvent tests

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class NumberOfEvents extends Component {
+  state = {
+    NumberOfEvent: 32
+  }
+
+  handleChange = event => {
+    const value = event.target.value;
+    this.setState({
+      NumberOfEvent: value
+    })
+  }
+  
   render() {
     return (
       <div>
-        
+        <input type="text" value={this.state.NumberOfEvent} className='number-of-event' onChange={this.handleChange} />
       </div>
     )
   }
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvent /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+  NumberOfEventsWrapper = shallow(<NumberOfEvents />)
+  });
+
+  test('render text input', () => {
+    expect(NumberOfEventsWrapper.find('.number-of-event')).toHaveLength(1)
+  });
+
+  test('update the state of NumberOfEvents', () => {
+    NumberOfEventsWrapper.setState({
+      NumberOfEvent: 5
+    });
+    const newNumberOfEvent = { target: { value: 5 }};
+    NumberOfEventsWrapper.find('.number-of-event').simulate('change', newNumberOfEvent);
+    expect(NumberOfEventsWrapper.state('NumberOfEvent')).toBe(5);
+  });
+})
