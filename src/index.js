6a8b0c8f3abe76080e405f3e46324a56c4e7ffac
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-12-06 14:55:47 +1300

    Add Atatus performance monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('7402f2d6dd664d68b54d6fa52bb9a1ac').install();
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
