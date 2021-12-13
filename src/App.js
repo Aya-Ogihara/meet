ccf99e5598f857ceddb444eb02084ceea9265403
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-12-13 18:18:32 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends React.Component {
     const code = searchParams.get('code');
 
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
+    // if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
           this.setState({ events, locations: extractLocations(events) });
@@ -74,7 +74,7 @@ class App extends React.Component {
           warningInfo: '',
         });
       }
-    }
+    // }
   }
 
   componentWillUnmount() {
