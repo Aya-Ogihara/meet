e2f77349fea13b59365f98c0539fa8fff30028b2
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-12-13 13:05:23 +1300

    Add service worker for PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+//import reportWebVitals from './reportWebVitals';
 
 import * as atatus from 'atatus-spa';
 atatus.config('7402f2d6dd664d68b54d6fa52bb9a1ac').install();
@@ -18,4 +19,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+serviceWorkerRegistration.register();
