eba44c89e5c95994ce74d1cad27ce90e54b57998
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-12-15 17:42:45 +1300

    Imprement WelcomeScreen logic

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ class App extends React.Component {
           });
         }
       });
-      
     }
   }
 
@@ -99,11 +98,6 @@ class App extends React.Component {
           updateNumberOfEvents={this.updateNumberOfEvents}
         />
         <WarningAlert text={this.state.warningInfo} />
-        {/* {navigator.onLine ? (
-          <WarningAlert text=' ' />
-        ) : (
-          <WarningAlert text='Warning: Your internet connection is offline' />
-        )} */}
         <EventList events={this.state.events} />
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
@@ -117,100 +111,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-// import React from 'react';
-// import './App.css';
-// import './nprogress.css';
-// import { extractLocations, getEvents } from './api';
-// import { ErrorAlert, WarningAlert } from './Alert';
-
-// // Component
-// import EventList from './EventList';
-// import CitySearch from './CitySearch';
-// import NumberOfEvents from './NumberOfEvents';
-
-// class App extends React.Component {
-//   state = {
-//     events: [],
-//     locations: [],
-//     numberOfEvents: 32,
-//     selectedLocation: 'all',
-//     errorInfo: '',
-//     //warningInfo: '',
-//   };
-
-//   updateEvents = (location, eventCount) => {
-//     getEvents().then((events) => {
-//       const locationEvents =
-//         location === 'all'
-//           ? events
-//           : events.filter((event) => event.location === location);
-
-//       const inputNumberOfEvents = locationEvents.slice(0, eventCount);
-//       this.setState({
-//         events: inputNumberOfEvents,
-//         selectedLocation: location,
-//       });
-//     });
-//   };
-
-//   updateNumberOfEvents = async (event) => {
-//     const value = event.target.value;
-//     if (value < 0 || value >= 32) {
-//       await this.setState({
-//         errorInfo: 'Please enter 1 - 32',
-//       });
-//     } else {
-//       await this.setState({
-//         numberOfEvents: value,
-//         errorInfo: '',
-//       });
-//       this.updateEvents(this.state.selectedLocation, this.state.numberOfEvents);
-//     }
-//   };
-
-//   componentDidMount() {
-//     this.mounted = true;
-//     getEvents().then((events) => {
-//       if (this.mounted) {
-//         this.setState({ events, locations: extractLocations(events) });
-//       }
-
-//       // if (!navigator.onLine) {
-//       //   this.setState({
-//       //     warningInfo: 'Warning: Your internet connection is offline',
-//       //   });
-//       // } else {
-//       //   this.setState({
-//       //     warningInfo: '',
-//       //   });
-//       // }
-//     });
-//   }
-
-//   componentWillUnmount() {
-//     this.mounted = false;
-//   }
-
-//   render() {
-//     return (
-//       <div className='App'>
-//         <h1 className='logo'>Meet App</h1>
-//         <CitySearch
-//           locations={this.state.locations}
-//           updateEvents={this.updateEvents}
-//         />
-//         <ErrorAlert text={this.state.errorInfo} />
-//         <NumberOfEvents
-//           numberOfEvents={this.state.numberOfEvents}
-//           updateNumberOfEvents={this.updateNumberOfEvents}
-//         />
-// {/* <WarningAlert text={this.state.warningInfo} /> */}
-//         { navigator.onLine ? (<WarningAlert text=' ' />) : (<WarningAlert text='Warning: Your connection is offline' />)}
-//         <EventList events={this.state.events} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
