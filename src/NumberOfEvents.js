eaebb7963a7a85d461c52521c67c1c7c9e5ef998
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-12-20 15:16:45 +1300

    Add label

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,14 +5,16 @@ export default class NumberOfEvents extends Component {
     return (
       <div className='number-of-events'>
         <h3>Number of Events to Display</h3>
+        <label>1  </label>
         <input
           type='range'
           min={1}
-          max={31}
+          max={30}
           value={this.props.numberOfEvents}
           className='number-of-event slider'
           onChange={(event) => this.props.updateNumberOfEvents(event)}
         />
+        <label>  30</label>
       </div>
     );
   }
